a6a379d33e5892a6b821b72d3b217f0c20812164
aliarnoos <alimarnoos@gmail.com>
2024-07-06 21:07:35 +0300

    feat: Create socket.io connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 import express from 'express';
-import { Peer } from "peerjs";
 import { createOnlineUsersTable } from './dev/devUtils';
+import http from 'http';
+import { Server } from 'socket.io';
+import { getOnlineUsers } from './services/dynamoDBservice';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-const peer = new Peer("pick-an-id");
+const server = http.createServer(app);
+const io = new Server(server);
 
 // This function is called in development only!
 createOnlineUsersTable()
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+
+  const sendOnlineUsers = async () => {
+    const onlineUsers = await getOnlineUsers();
+    const connectionIds = onlineUsers.map(user => user.connectionId);
+    socket.emit('update', connectionIds);
+  };
+
+  sendOnlineUsers();
+
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
